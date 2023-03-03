e127301b35300e494c4e0b926f9988e9dddf8d12
Hyunjeong <113197284+HyunjeongJang@users.noreply.github.com>
2023-03-03 16:22:20 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -12,4 +12,20 @@ class Solution {
         }         
         return answer;
     }
+    
+    
+//     public int GCD(int num1, int num2) {
+//         if (num1 % num2 == 0)
+//             return num2;
+//         return GCD(num2, num1 % num2);
+//     }
+
+//     public int LCM(int num1, int num2) {
+//         return num1 * num2 / GCD(num1, num2);
+//     }
+
+//     public int solution(int n) {
+//         return LCM(n, 6) / 6;
+//     }
+    
 }
