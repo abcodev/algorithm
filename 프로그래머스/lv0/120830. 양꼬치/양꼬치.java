7ebe361c307f3a14b47ff1c6f1e59e7eab8f1d5a
HyunjeongJang <113197284+HyunjeongJang@users.noreply.github.com>
2023-05-02 15:45:17 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,10 @@
 class Solution {
+    // public int solution(int n, int k) {
+    //     int answer = 12000 * n + (k - (n/10))*2000;
+    //     return answer;
+    // }
+    
     public int solution(int n, int k) {
-        int answer = 0;
-        
-        answer = 12000 * n + (k - (n/10))*2000;
-        return answer;
+        return n * 12000 + k * 2000 - (n / 10 * 2000);
     }
 }
