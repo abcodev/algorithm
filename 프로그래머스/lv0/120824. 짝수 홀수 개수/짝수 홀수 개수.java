3bdb192a77808940048b49d9ecb008ce052f4140
HyunjeongJang <113197284+HyunjeongJang@users.noreply.github.com>
2023-05-12 21:20:56 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 2.68 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,14 +1,28 @@
+import java.util.stream.IntStream;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
-        answer = new int[2];
-        for(int i=0; i<num_list.length; i++) {
-            if(num_list[i]%2==0) {
-                answer[0]++;
-            }else {
-                answer[1]++;
-            }
-        }        
-        return answer;
+        // 1번
+        // int[] answer = {};
+        // answer = new int[2];
+        // for(int i=0; i<num_list.length; i++) {
+        //     if(num_list[i]%2==0) {
+        //         answer[0]++;
+        //     }else {
+        //         answer[1]++;
+        //     }
+        // }        
+        // return answer;
+        
+        // 2번
+        // int[] answer = new int[2];
+        // for(int i = 0; i < num_list.length; i++)
+        //     answer[num_list[i] % 2]++;
+        // return answer;
+        
+        // 3번
+        return IntStream.of((int) Arrays.stream(num_list).filter(i -> i % 2 == 0).count(), (int) Arrays.stream(num_list).filter(i -> i % 2 == 1).count()).toArray();
+
     }
 }
