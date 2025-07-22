7e56a15d84a9f2314aacebc918a82303eb52efe8
hyunjeong <113197284+abcodev@users.noreply.github.com>
2025-07-22 11:22:09 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int N) {
+        int sum = 0;
+        while (N > 0) {
+            sum += N % 10;
+            N /= 10;
+        }
+        return sum;
+    }
+}
